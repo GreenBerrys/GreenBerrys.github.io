c80088fdb3a4f97b118eb6ef0476d5fd56c704a7
Frank <fhermann@posteo.de>
2024-08-18 20:38:07 +0200

    updated

--- mp4streamer/mkJSONbase.bat
+++ mp4streamer/mkJSONbase.bat
@@ -1,5 +1,7 @@
 @echo off
+set NODE_ENV=production
 %~d0
 cd \mp4streamer\backend
 ..\node mkJSONbase.js
 cd \
+pause
--- mp4streamer/mp4streamer.bat
+++ mp4streamer/mp4streamer.bat
@@ -1,4 +1,5 @@
 @echo off
+set NODE_ENV=production
 %~d0
 cd \mp4streamer\backend
 if not exist database\movies.json (
